30c6f426ac7b7a7cd71695e62a8eba371a02aa1f
Juan <juan@mantis.uy>
2025-11-21 14:14:26 -0300

    fix boton por props desde app

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,35 +2,33 @@ import { useState } from "react";
 import "../Card/Card.css";
 import Button from "../Button/Button";
 
+const Card = ({ textCard, titulo, txtBtn }) => {
+  const [clicked, setClicked] = useState(false);
 
-const Card = ({textCard, titulo}) => {
-    const [clicked, setClicked] = useState(false);
-
-    const handleClick = () => {
+  const handleClick = () => {
     setClicked(!clicked);
-            
-    };
+  };
 
-    return (
-        <>
-        <section className="card">
-            <div className="card-text">
-                <h1>{titulo}</h1>
-                <p>{textCard}</p>
-                  {clicked && (
-          <p className="extra-text">
-            rem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry standard dummy text
-            ever since the 1500s...
-          </p>
-        )}
-            </div>
-            <div className="card-button">
-                <Button text= 'Ver mas...'onClick={handleClick} />
-            </div>
-        </section>
-        </>
-    )
+  return (
+    <>
+      <section className="card">
+        <div className="card-text">
+          <h1>{titulo}</h1>
+          <p>{textCard}</p>
+          {clicked && (
+            <p className="extra-text">
+              rem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry standard dummy text
+              ever since the 1500s...
+            </p>
+          )}
+        </div>
+        <div className="card-button">
+          <Button text={txtBtn} onClick={handleClick} />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Card;
